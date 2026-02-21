807f741f3729272a2acb56116986a0a0dc0d481e
Glen Qin <67018349+glenqin916@users.noreply.github.com>
2026-02-20 18:16:35 -0700

    Update part1.py

--- part1/part1.py
+++ part1/part1.py
@@ -18,12 +18,15 @@ PORT = 5000
 
 # This script runs automatically when the VM boots
 STARTUP_SCRIPT = """#!/bin/bash
-apt-get update
-apt-get install -y python3-pip git
-git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
-cd python-docs-samples/appengine/standard_python3/hello_world
-pip3 install -r requirements.txt
-python3 main.py --host=0.0.0.0 --port=5000 &
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip git
+git clone https://github.com/cu-csci-4253-datacenter/flask-tutorial
+cd flask-tutorial
+sudo python3 setup.py install
+sudo pip3 install -e .
+export FLASK_APP=flaskr
+flask init-db
+nohup flask run -h 0.0.0.0 &
 """
 
 #
